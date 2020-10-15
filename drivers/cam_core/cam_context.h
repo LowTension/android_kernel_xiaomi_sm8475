2ebae4fabe652262d67ed9114d1b2764a0ce5429
Alok Chauhan <alokc@codeaurora.org>
2020-10-15 20:32:53 +0530

    msm: camera: req_mgr: Enhance camera v4l2 subdev shutdown sequence
    when provider crash occurred, there was a CSID lane overflow
    observed due to shutdown sequence from CRM(sensor, csid, csiphy).
    To fix the issue need to change the shutdown sequence
    (csid, sensor, csiphy).
    
    This change will update the devices sequence in order while registering
    and close the sequence accordingly.
    
    CRs-Fixed: 2852076
    Change-Id: Ia6d8022e995823bf031400c33528eb8544dc2e29
    Signed-off-by: Alok Chauhan <alokc@codeaurora.org>

--- drivers/cam_core/cam_context.h
+++ drivers/cam_core/cam_context.h
@@ -9,6 +9,7 @@
 #include <linux/mutex.h>
 #include <linux/spinlock.h>
 #include <linux/kref.h>
+#include <media/v4l2-subdev.h>
 #include "cam_req_mgr_interface.h"
 #include "cam_hw_mgr_intf.h"
 #include "cam_smmu_api.h"
@@ -97,6 +98,7 @@ struct cam_ctx_request {
  * @acquire_hw:            Function pointer for acquire hw
  * @release_hw:            Function pointer for release hw
  * @dump_dev:              Function pointer for dump dev
+ * @shutdown_dev:          Function pointer for shutdown dev
  *
  */
 struct cam_ctx_ioctl_ops {
@@ -116,6 +118,8 @@ struct cam_ctx_ioctl_ops {
 	int (*release_hw)(struct cam_context *ctx, void *args);
 	int (*dump_dev)(struct cam_context *ctx,
 			struct cam_dump_req_cmd *cmd);
+	int (*shutdown_dev)(struct v4l2_subdev *sd,
+			struct v4l2_subdev_fh *fh);
 };
 
 /**
@@ -482,6 +486,19 @@ int cam_context_handle_start_dev(struct cam_context *ctx,
  */
 int cam_context_handle_stop_dev(struct cam_context *ctx,
 		struct cam_start_stop_dev_cmd *cmd);
+
+/**
+ * cam_context_handle_shutdown_dev()
+ *
+ * @brief:        Handle shutdown device command
+ *
+ * @ctx:          Object pointer for cam_context
+ * @cmd:          Shutdown device command payload
+ * @fh:           Pointer to struct v4l2_subdev_fh
+ *
+ */
+int cam_context_handle_shutdown_dev(struct cam_context *ctx,
+	struct cam_control *cmd, struct v4l2_subdev_fh *fh);
 
 /**
  * cam_context_handle_dump_dev()
