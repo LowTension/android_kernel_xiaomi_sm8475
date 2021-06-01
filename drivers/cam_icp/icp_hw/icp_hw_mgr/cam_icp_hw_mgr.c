8432cd49d1445cd0b4d63d05670b0d76c1562fb1
Pavan Kumar Chilamkurthi <pchilamk@codeaurora.org>
2021-05-31 19:59:21 -0700

    msm: camera: cpas: Add cpas support for camera v780 platform
    Add register and qos header for v780.
    
    CRs-Fixed: 2948116
    Change-Id: I486bac6987187cdba8851b8a81a4cc8be9b2cd64
    Signed-off-by: Pavan Kumar Chilamkurthi <pchilamk@codeaurora.org>

--- drivers/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c
+++ drivers/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c
@@ -6444,7 +6444,8 @@ int cam_icp_hw_mgr_init(struct device_node *of_node, uint64_t *hw_mgr_hdl,
 	if ((camera_hw_version == CAM_CPAS_TITAN_480_V100) ||
 		(camera_hw_version == CAM_CPAS_TITAN_580_V100) ||
 		(camera_hw_version == CAM_CPAS_TITAN_570_V200) ||
-		(camera_hw_version == CAM_CPAS_TITAN_680_V100)) {
+		(camera_hw_version == CAM_CPAS_TITAN_680_V100) ||
+		(camera_hw_version == CAM_CPAS_TITAN_780_V100)) {
 		if (cam_caps & CPAS_TITAN_IPE0_CAP_BIT)
 			icp_hw_mgr.ipe0_enable = true;
 		if (cam_caps & CPAS_BPS_BIT)
