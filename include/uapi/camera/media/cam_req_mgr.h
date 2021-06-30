e2c7402dfd8ec61359eba2fb30fa9b3613d4e980
Rishabh Jain <risjai@codeaurora.org>
2021-06-30 18:38:24 +0530

    msm: camera: memmgr: Add support for non pix secure buffers
    Add support to allocate such buffers which need to accessed
    by non-pix VM.
    
    CRs-Fixed: 2981372
    Change-Id: Iac965a479e05d638c6323bfacd7c6a4d89f2a223
    Signed-off-by: Rishabh Jain <risjai@codeaurora.org>

--- include/uapi/camera/media/cam_req_mgr.h
+++ include/uapi/camera/media/cam_req_mgr.h
@@ -290,6 +290,7 @@ struct cam_req_mgr_link_control {
 #define CAM_MEM_FLAG_CDSP_OUTPUT                (1<<12)
 #define CAM_MEM_FLAG_DISABLE_DELAYED_UNMAP      (1<<13)
 #define CAM_MEM_FLAG_KMD_DEBUG_FLAG             (1<<14)
+#define CAM_MEM_FLAG_EVA_NOPIXEL                (1<<15)
 
 
 #define CAM_MEM_MMU_MAX_HANDLE                  16
