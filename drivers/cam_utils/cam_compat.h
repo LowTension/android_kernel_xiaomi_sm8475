ee84a3df7996db8ccb6c0b865ee324ac805d37fa
Karthik Jayakumar <kjayakum@codeaurora.org>
2021-10-06 16:58:22 -0700

    msm: camera: mem_mgr: Add dma_buf_vmap to cam_compat
    dma_buf_vmap function signature changed between kernel version
    5.10 and 5.15.
    
    CRs-Fixed: 3048249
    Change-Id: I4c8dc951cc383c3dc888fca1d0b1becf92f4662c
    Signed-off-by: Karthik Jayakumar <kjayakum@codeaurora.org>

--- drivers/cam_utils/cam_compat.h
+++ drivers/cam_utils/cam_compat.h
@@ -47,6 +47,7 @@ void cam_free_clear(const void *);
 void cam_check_iommu_faults(struct iommu_domain *domain,
 	struct cam_smmu_pf_info *pf_info);
 int cam_get_ddr_type(void);
+int cam_compat_util_get_dmabuf_va(struct dma_buf *dmabuf, uintptr_t *vaddr);
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
 int cam_req_mgr_ordered_list_cmp(void *priv,
