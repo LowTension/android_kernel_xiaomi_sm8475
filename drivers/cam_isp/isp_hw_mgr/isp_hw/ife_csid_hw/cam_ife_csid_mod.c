b08058de6f6623f78c5f73003c139836b211cb2d
Alok Chauhan <alokc@codeaurora.org>
2021-10-18 14:04:40 +0530

    msm: camera: isp: Add header file VFE and CSID for Cape
    This commit adds VFE/CSID header file changes for titan
    version 680, hw version 110 hardware.
    
    CRs-Fixed: 3063982
    Change-Id: I038b1e2f0156e70874ec941b7d34df6853d4ddd7
    Signed-off-by: Alok Chauhan <alokc@codeaurora.org>

--- drivers/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_mod.c
+++ drivers/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_mod.c
@@ -17,6 +17,7 @@
 #include "cam_ife_csid570.h"
 #include "cam_ife_csid580.h"
 #include "cam_ife_csid680.h"
+#include "cam_ife_csid680_110.h"
 #include "cam_ife_csid780.h"
 
 #define CAM_CSID_DRV_NAME                    "csid"
@@ -66,6 +67,11 @@ static struct cam_ife_csid_core_info cam_ife_csid680_hw_info = {
 	.sw_version  = CAM_IFE_CSID_VER_2_0,
 };
 
+static struct cam_ife_csid_core_info cam_ife_csid680_110_hw_info = {
+	.csid_reg = &cam_ife_csid_680_reg_info,
+	.sw_version  = CAM_IFE_CSID_VER_2_0,
+};
+
 static struct cam_ife_csid_core_info cam_ife_csid780_hw_info = {
 	.csid_reg = &cam_ife_csid_780_reg_info,
 	.sw_version  = CAM_IFE_CSID_VER_2_0,
@@ -108,6 +114,10 @@ static const struct of_device_id cam_ife_csid_dt_match[] = {
 	{
 		.compatible = "qcom,csid680",
 		.data = &cam_ife_csid680_hw_info,
+	},
+	{
+		.compatible = "qcom,csid680_110",
+		.data = &cam_ife_csid680_110_hw_info,
 	},
 	{
 		.compatible = "qcom,csid780",
