d38a07c750e3a4b6c0b5cc6e6c84910e9675d15b
Depeng Shao <quic_depengs@quicinc.com>
2021-12-16 22:34:49 +0800

    msm: camera: sensor: enable the compilation for flash
    This change enables the compilation for flash even
    though there is no pmic flash support, since the i2c
    flash still needs the flash framework.
    
    CRs-Fixed: 3095773
    Change-Id: I220a38dce1cbc829e446a65c61b7b7eb6b3a1c5b
    Signed-off-by: Depeng Shao <quic_depengs@quicinc.com>

--- drivers/camera_main.c
+++ drivers/camera_main.c
@@ -24,11 +24,7 @@
 #include "cam_eeprom_dev.h"
 #include "cam_ois_dev.h"
 #include "cam_tpg_dev.h"
-
-#if IS_REACHABLE(CONFIG_LEDS_QPNP_FLASH_V2) || \
-	IS_REACHABLE(CONFIG_LEDS_QTI_FLASH)
 #include "cam_flash_dev.h"
-#endif
 
 #include "a5_core.h"
 #include "lx7_dev.h"
@@ -116,10 +112,7 @@ static const struct camera_submodule_component camera_sensor[] = {
 	{&cam_sensor_driver_init, &cam_sensor_driver_exit},
 	{&cam_eeprom_driver_init, &cam_eeprom_driver_exit},
 	{&cam_ois_driver_init, &cam_ois_driver_exit},
-#if IS_REACHABLE(CONFIG_LEDS_QPNP_FLASH_V2) || \
-	IS_REACHABLE(CONFIG_LEDS_QTI_FLASH)
 	{&cam_flash_init_module, &cam_flash_exit_module},
-#endif
 #endif
 };
 
