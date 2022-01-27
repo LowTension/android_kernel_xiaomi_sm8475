f874cdea19fa3d24981fdbb52b0170441c2ec251
sokchetra eung <quic_eung@quicinc.com>
2022-01-27 11:10:40 -0800

    msm: camera: common: Dynamically allocated flush request array
    Change flush request active and flush request pending arrays from
    static of size 20 to dynamically allocated size based on current
    pending and active request lists.
    
    CRs-Fixed: 3046003
    Change-Id: Iaa1179881d786359dbe9d37411a6ef3ae8038227
    Signed-off-by: sokchetra eung <quic_eung@quicinc.com>

--- drivers/cam_core/cam_hw_mgr_intf.h
+++ drivers/cam_core/cam_hw_mgr_intf.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _CAM_HW_MGR_INTF_H_
@@ -326,9 +327,9 @@ struct cam_hw_config_args {
 struct cam_hw_flush_args {
 	void                           *ctxt_to_hw_map;
 	uint32_t                        num_req_pending;
-	void                           *flush_req_pending[20];
+	void                          **flush_req_pending;
 	uint32_t                        num_req_active;
-	void                           *flush_req_active[20];
+	void                          **flush_req_active;
 	enum flush_type_t               flush_type;
 	uint32_t                        last_flush_req;
 };
