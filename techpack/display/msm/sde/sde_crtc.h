7e780378808f0fd711d1d9f035da226502b977b2
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-10 16:00:30 +0200

    techpack: display: sde_crtc: setup dim layer
    Change-Id: I267c6692374f368e6adb31bf28692330130cd720

--- techpack/display/msm/sde/sde_crtc.h
+++ techpack/display/msm/sde/sde_crtc.h
@@ -507,6 +507,7 @@ struct sde_crtc_state {
 	uint32_t num_dim_layers;
 	uint32_t cwb_enc_mask;
 	struct sde_hw_dim_layer dim_layer[SDE_MAX_DIM_LAYERS];
+	struct sde_hw_dim_layer *fod_dim_layer;
 	uint32_t num_ds;
 	uint32_t num_ds_enabled;
 	struct sde_hw_ds_cfg ds_cfg[SDE_MAX_DS_COUNT];
